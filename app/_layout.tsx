2c0e8e93dd9503dc5d3f8a1560379baad8cb9609
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-10-16 18:19:16 +0530

    added font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
+import { HeaderTitle } from "@react-navigation/elements";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    "Satoshi-Light": require(".././assets/fonts/Satoshi-Light.otf"),
+    'Satoshi': require(".././assets/fonts/Satoshi-Regular.otf"),
+    "Satoshi-Bold": require(".././assets/fonts/Satoshi-Bold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
