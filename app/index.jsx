114115de2bffddf4adf63fed9a52ba1d30ffdc6b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-10-17 08:07:34 +0530

    graph rendering

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
-import {Canvas, LinearGradient, vec} from "@shopify/react-native-skia"
+import { Canvas, LinearGradient, Path, vec, Skia } from "@shopify/react-native-skia"
+import { GenerateStringPath } from "../data/math-stuff"
+
+const COLORS = ["#ec38bc", "#3e0174"]
+
+const path = GenerateStringPath("curveBasis", "today", 300)
+console.log(path)
+const skpath = Skia.Path.MakeFromSVGString(path)
+
 
 export default function Index() {
   return (
@@ -10,17 +18,24 @@ export default function Index() {
         paddingVertical: 30,
         alignItems: "center",
         backgroundColor: "black",
+        fontFamily:"Satoshi-Light"
       }}
     >
       <Text style={styles.home__price}>$10,000.12</Text>
 
 
-      <Canvas >
-        <LinearGradient
-          start={vec(0,0)}
-          end={vec(300, 300)}
-          colors={}
-        />
+      <Canvas style={{ width: 300, height: 300, marginTop:30 }}>
+        <Path path={path}
+          style="stroke"
+          strokeWidth={4}
+          color={"#fff"}>
+
+          <LinearGradient
+            start={vec(0, 0)}
+            end={vec(300, 300)}
+            colors={COLORS}
+          />
+        </Path>
       </Canvas>
 
     </View>
@@ -29,9 +44,8 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   home__price: {
-    color: "white",
-    fontSize: 48,
-    fontWeight: "800",
+    color: "#ffffab",
+    fontSize: 52,
     fontFamily: 'Satoshi-Bold'
   },
 });
