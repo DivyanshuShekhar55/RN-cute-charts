0bff83e575f4a63b43ba4b9b37948a0111ec1a1a
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-10-19 23:12:39 +0530

    fixed cursor code

--- components/Cursor.jsx
+++ components/Cursor.jsx
@@ -4,20 +4,20 @@ import { Canvas, Circle, Group } from '@shopify/react-native-skia'
 import { useDerivedValue } from 'react-native-reanimated'
 
 const Cursor = ({ x_pos, y_pos }) => {
+  console.log("into cursor")
 
-  const transform = useDerivedValue(() => {
-    return [
-      { translateX: x_pos.value },
-      { translateY: y_pos.value }
-    ]
-  }, [x_pos, y_pos])
+  console.log("transform", x_pos.value, y_pos.value)
 
   return (
 
-    <Group transform={transform}>
-      <Circle style="fill" color="#f69d69" cx={50} cy={50} r={5} />
-      <Circle style="stroke" color="#f69d69" cx={50} cy={50} r={12} strokeWidth={2} opacity={0.65} />
-      <Circle style="stroke" color="#f69d69" cx={50} cy={50} r={18} strokeWidth={2} opacity={0.65} />
+    <Group transform={[
+      { translateX: x_pos.value },
+      { translateY: y_pos.value }]}
+    >
+
+      <Circle style="fill" color="#f69d69" cx={0} cy={0} r={5} />
+      <Circle style="stroke" color="#f69d69" cx={0} cy={0} r={12} strokeWidth={2} opacity={0.65} />
+      <Circle style="stroke" color="#f69d69" cx={0} cy={0} r={18} strokeWidth={2} opacity={0.65} />
     </Group>
 
   )
--- data/data.js
+++ data/data.js
@@ -1,91 +1,90 @@
 const daily_data = [
   {
-    "id": 1,
-    "timestamp": "2025-09-23T09:12:13",
-    "price": 131.94
+    id: 1,
+    timestamp: "2025-09-23T09:12:13",
+    price: 131.94,
   },
   {
-      "id": 2,
-    "timestamp": "2025-09-23T09:41:25",
-    "price": 134.58
+    id: 2,
+    timestamp: "2025-09-23T09:41:25",
+    price: 134.58,
   },
   {
-    "id": 3,
-    "timestamp": "2025-09-23T10:00:00",
-    "price": 148.25
+    id: 3,
+    timestamp: "2025-09-23T10:00:00",
+    price: 148.25,
   },
   {
-    "id": 4,
-    "timestamp": "2025-09-23T10:42:01",
-    "price": 160.15
+    id: 4,
+    timestamp: "2025-09-23T10:42:01",
+    price: 160.15,
   },
   {
-    "id": 5,
-    "timestamp": "2025-09-23T11:50:02",
-    "price": 156.53
+    id: 5,
+    timestamp: "2025-09-23T11:50:02",
+    price: 156.53,
   },
   {
-    "id": 6,
-    "timestamp": "2025-09-23T12:40:16",
-    "price": 117.81
+    id: 6,
+    timestamp: "2025-09-23T12:40:16",
+    price: 117.81,
   },
   {
-    "id": 7,
-    "timestamp": "2025-09-23T12:49:40",
-    "price": 189.28
+    id: 7,
+    timestamp: "2025-09-23T12:49:40",
+    price: 189.28,
   },
   {
-    "id": 8,
-    "timestamp": "2025-09-23T13:17:21",
-    "price": 157.18
+    id: 8,
+    timestamp: "2025-09-23T13:17:21",
+    price: 157.18,
   },
   {
-    "id": 9,
-    "timestamp": "2025-09-23T13:30:02",
-    "price": 141.88
+    id: 9,
+    timestamp: "2025-09-23T13:30:02",
+    price: 141.88,
   },
   {
-    "id": 10,
-    "timestamp": "2025-09-23T14:12:31",
-    "price": 160.1
+    id: 10,
+    timestamp: "2025-09-23T14:12:31",
+    price: 160.1,
   },
   {
-    "id": 11,
-    "timestamp": "2025-09-23T14:05:14",
-    "price": 103.45
+    id: 11,
+    timestamp: "2025-09-23T14:05:14",
+    price: 103.45,
   },
   {
-    "id": 12,
-    "timestamp": "2025-09-23T14:27:50",
-    "price": 85.16
+    id: 12,
+    timestamp: "2025-09-23T14:27:50",
+    price: 85.16,
   },
   {
-    "id": 13,
-    "timestamp": "2025-09-23T14:54:48",
-    "price": 131.38
+    id: 13,
+    timestamp: "2025-09-23T14:54:48",
+    price: 131.38,
   },
   {
-    "id": 14,
-    "timestamp": "2025-09-23T15:49:31",
-    "price": 126.39
+    id: 14,
+    timestamp: "2025-09-23T15:49:31",
+    price: 126.39,
   },
   {
-    "id": 15,
-    "timestamp": "2025-09-23T16:16:16",
-    "price": 147.56
+    id: 15,
+    timestamp: "2025-09-23T16:16:16",
+    price: 147.56,
   },
   {
-    "id": 16,
-    "timestamp": "2025-09-23T16:59:30",
-    "price": 143.53
+    id: 16,
+    timestamp: "2025-09-23T16:59:30",
+    price: 143.53,
   },
   {
-    "id": 17,
-    "timestamp": "2025-09-23T16:39:50",
-    "price": 85.49
-  }
-]
-
+    id: 17,
+    timestamp: "2025-09-23T16:39:50",
+    price: 85.49,
+  },
+];
 
 const weekly_data = [
   {
@@ -132,4 +131,4 @@ const weekly_data = [
   },
 ];
 
-export default  daily_data ;
+export default daily_data;
