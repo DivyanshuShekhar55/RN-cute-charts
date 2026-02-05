32394d95bdc9d5f5d2966a195dca0a9744d5b617
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-02-05 11:29:27 +0530

    some non impo changes to main

--- app/CandleMain.jsx
+++ app/CandleMain.jsx
@@ -1,13 +1,13 @@
 import { View } from 'react-native'
-import { ChartScrub  as CandleScrub} from "../components/CandleStick"
+import { ChartScrub as CandleScrub } from "../components/CandleStick"
 import { candle_data } from "../data/candledata"
 
 const data = candle_data.slice(0, 20)
 
 const CandleMain = () => {
   return (
-    <View style={{alignItems: "center", justifyContent: "center", flex: 1}}>
-      <CandleScrub width={400} height={500} fill={["green", "red"]} bgCol={"white"} data={data} wickColor='gray' crossHairColor='gray'/>
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <CandleScrub width={400} height={500} fill={["green", "red"]} bgCol={"white"} data={data} wickColor='gray' crossHairColor='gray' />
     </View>
   )
 }
